af2a88b513afc7a1ba07cd7f6b887c7777753863
派了个萌 <pylogmon@outlook.com>
2023-05-07 18:15:34 +0800

    update icons

--- src/pages/download.js
+++ src/pages/download.js
@@ -6,9 +6,12 @@ import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import { BsWindows } from 'react-icons/bs';
+import { BsApple } from 'react-icons/bs';
+import { FaLinux } from 'react-icons/fa';
 
 export default function Download() {
-    const [latestVersion, setLatestVersion] = useState();
+    const [latestVersion, setLatestVersion] = useState('0.2.4');
     const {
         siteConfig: { customFields },
     } = useDocusaurusContext();
@@ -37,67 +40,104 @@ export default function Download() {
                         <div style={{ display: 'inline-block', margin: 'auto' }}>
                             <Link
                                 className="button button--primary button--lg"
-                                style={{ marginTop: '25px', width: 300 }}
+                                style={{ marginTop: '25px', width: 350 }}
                                 to={latestVersion ? `https://gh.pylogmon.cn/https://github.com/Pylogmon/pot/releases/latest/download/pot_${latestVersion}_x64_zh-CN.msi` : 'https://github.com/Pylogmon/pot/releases/latest'}>
-                                Windows x64(.msi)
-                                <br />
-                                {latestVersion}
+                                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                                    <BsWindows style={{ marginTop: 'auto', marginBottom: 'auto' }} />
+                                    <span>
+                                        Windows x64(.msi)
+                                        <br />
+                                        {latestVersion}
+                                    </span>
+                                </div>
                             </Link>
                             <br />
                             <Link
                                 className="button button--primary button--lg"
-                                style={{ marginTop: '25px', width: 300 }}
+                                style={{ marginTop: '25px', width: 350 }}
                                 to={latestVersion ? `https://gh.pylogmon.cn/https://github.com/Pylogmon/pot/releases/latest/download/pot_${latestVersion}_x64_portable.zip` : 'https://github.com/Pylogmon/pot/releases/latest'}>
-                                Windows x64(portable)
-                                <br />
-                                {latestVersion}
+                                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                                    <BsWindows style={{ marginTop: 'auto', marginBottom: 'auto' }} />
+                                    <span>
+                                        Windows x64(portable)
+                                        <br />
+                                        {latestVersion}
+                                    </span>
+                                </div>
                             </Link>
                         </div>
                         <div style={{ display: 'inline-block', margin: 'auto' }}>
                             <Link
                                 className="button button--primary button--lg"
-                                style={{ marginTop: '25px', width: 300 }}
+                                style={{ marginTop: '25px', width: 350 }}
                                 to={latestVersion ? `https://gh.pylogmon.cn/https://github.com/Pylogmon/pot/releases/latest/download/pot_${latestVersion}_aarch64.dmg` : 'https://github.com/Pylogmon/pot/releases/latest'}>
-                                MacOS aarch64(.dmg)
-                                <br />
-                                {latestVersion}
+                                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                                    <BsApple style={{ marginTop: 'auto', marginBottom: 'auto' }} />
+                                    <span>
+                                        MacOS aarch64(.dmg)
+                                        <br />
+                                        {latestVersion}
+                                    </span>
+                                </div>
                             </Link>
                             <br />
                             <Link
                                 className="button button--primary button--lg"
-                                style={{ marginTop: '25px', width: 300 }}
+                                style={{ marginTop: '25px', width: 350 }}
                                 to={latestVersion ? `https://gh.pylogmon.cn/https://github.com/Pylogmon/pot/releases/latest/download/pot_${latestVersion}_x64.dmg` : 'https://github.com/Pylogmon/pot/releases/latest'}>
-                                MacOS x64(.dmg)
-                                <br />
-                                {latestVersion}
+
+                                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                                    <BsApple style={{ marginTop: 'auto', marginBottom: 'auto' }} />
+                                    <span>
+                                        MacOS x64(.dmg)
+                                        <br />
+                                        {latestVersion}
+                                    </span>
+                                </div>
                             </Link>
                         </div>
                         <div style={{ display: 'inline-block', margin: 'auto' }}>
                             <Link
                                 className="button button--primary button--lg"
-                                style={{ marginTop: '25px', width: 300 }}
+                                style={{ marginTop: '25px', width: 350 }}
                                 to={latestVersion ? `https://gh.pylogmon.cn/https://github.com/Pylogmon/pot/releases/latest/download/pot_${latestVersion}_amd64.deb` : 'https://github.com/Pylogmon/pot/releases/latest'}>
-                                Linux x64(.deb)
-                                <br />
-                                {latestVersion}
+
+                                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                                    <FaLinux style={{ marginTop: 'auto', marginBottom: 'auto' }} />
+                                    <span>
+                                        Linux x64(.deb)
+                                        <br />
+                                        {latestVersion}
+                                    </span>
+                                </div>
                             </Link>
                             <br />
                             <Link
                                 className="button button--primary button--lg"
-                                style={{ marginTop: '25px', width: 300 }}
+                                style={{ marginTop: '25px', width: 350 }}
                                 to={latestVersion ? `https://gh.pylogmon.cn/https://github.com/Pylogmon/pot/releases/latest/download/pot_${latestVersion}_amd64_universal.deb` : 'https://github.com/Pylogmon/pot/releases/latest'}>
-                                Linux x64 universal(.deb)
-                                <br />
-                                {latestVersion}
+                                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                                    <FaLinux style={{ marginTop: 'auto', marginBottom: 'auto' }} />
+                                    <span>
+                                        Linux x64 universal(.deb)
+                                        <br />
+                                        {latestVersion}
+                                    </span>
+                                </div>
                             </Link>
                             <br />
                             <Link
                                 className="button button--primary button--lg"
-                                style={{ marginTop: '25px', width: 300 }}
+                                style={{ marginTop: '25px', width: 350 }}
                                 to={latestVersion ? `https://gh.pylogmon.cn/https://github.com/Pylogmon/pot/releases/latest/download/pot_${latestVersion}_amd64.AppImage` : 'https://github.com/Pylogmon/pot/releases/latest'}>
-                                Linux x64(.AppImage)
-                                <br />
-                                {latestVersion}
+                                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                                    <FaLinux style={{ marginTop: 'auto', marginBottom: 'auto' }} />
+                                    <span>
+                                        Linux x64(.AppImage)
+                                        <br />
+                                        {latestVersion}
+                                    </span>
+                                </div>
                             </Link>
                         </div>
                     </div>
