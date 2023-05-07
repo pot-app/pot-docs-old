f59773bd471ac1327c9c0a12b8cc1c5845878618
派了个萌 <pylogmon@outlook.com>
2023-05-07 13:02:55 +0800

    Update Github Icon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -77,6 +77,7 @@ const config = {
           {
             label: "GitHub",
             href: "https://github.com/Pylogmon/pot",
+            className: 'navbarIcon gitHubIcon',
             position: "right",
           },
         ],
