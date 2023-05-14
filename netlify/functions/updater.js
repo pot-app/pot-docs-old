14d876b0f7c09e377c17bb41144ed76238a713f1
派了个萌 <pylogmon@outlook.com>
2023-05-14 11:55:53 +0800

    Update

---
+++ netlify/functions/updater.js
@@ -0,0 +1,11 @@
+export const handler = async () => {
+    const update_json = await fetch('https://github.com/pot-app/pot-desktop/releases/download/updater/update.json', {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/octet-stream' },
+    });
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(await update_json.text()),
+    }
+}
