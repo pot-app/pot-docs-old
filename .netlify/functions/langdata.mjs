614dbf95c3b5d7972153c591ec8b6623d614b24a
Pylogmon <pylogmon@outlook.com>
2023-06-06 14:51:19 +0800

    Update

---
+++ .netlify/functions/langdata.mjs
@@ -0,0 +1,23 @@
+import fetch from "node-fetch";
+
+const url = 'https://tessdata.projectnaptha.com/4.0.0/';
+
+export const handler = async (event, context) => {
+    const path = event.path;
+    console.log(path);
+    try {
+        const response = await fetch(url + path, {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/octet-stream' },
+        });
+
+        const data = await response.text();
+        return { statusCode: 200, body: data };
+    } catch (error) {
+        console.log(error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Failed fetching data' }),
+        };
+    }
+};
