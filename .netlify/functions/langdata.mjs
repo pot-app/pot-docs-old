eec0fa73f5bfb2c6c74c0519620b7824a29ed6fe
Pylogmon <pylogmon@outlook.com>
2023-06-06 14:55:04 +0800

    Update

--- .netlify/functions/langdata.mjs
+++ .netlify/functions/langdata.mjs
@@ -4,9 +4,10 @@ const url = 'https://tessdata.projectnaptha.com/4.0.0/';
 
 export const handler = async (event, context) => {
     const path = event.path;
+
     console.log(path);
     try {
-        const response = await fetch(url + path, {
+        const response = await fetch(url + path.split('/')[-1], {
             method: 'GET',
             headers: { 'Content-Type': 'application/octet-stream' },
         });
