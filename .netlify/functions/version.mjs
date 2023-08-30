cc523787647b28f76cacb5564cd8e05057501285
Pylogmon <pylogmon@outlook.com>
2023-08-30 12:21:32 +0800

    Update

---
+++ .netlify/functions/version.mjs
@@ -0,0 +1,21 @@
+import fetch from "node-fetch";
+
+const url = 'https://api.github.com/repos/pot-app/pot-desktop/releases/tags/updater';
+
+export const handler = async (event, context) => {
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: { Authorization: `Bearer ${process.env.GITHUB_TOKEN}` },
+        });
+
+        const res = await response.json();
+        return { statusCode: 200, body: res };
+    } catch (error) {
+        console.log(error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Failed fetching data' }),
+        };
+    }
+};
