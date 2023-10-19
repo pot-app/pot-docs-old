f32b097f9a2bbe59019c9f771a7be6e7541d3288
Pylogmon <pylogmon@outlook.com>
2023-10-19 20:53:55 +0800

    Update baidu.js

--- static/js/baidu.js
+++ static/js/baidu.js
@@ -1,5 +1,6 @@
 var _hmt = _hmt || [];
 (function() {
+  console.log('baidu');
   var hm = document.createElement("script");
   hm.src = "https://hm.baidu.com/hm.js?3132de7130a225150415222801ddb498";
   var s = document.getElementsByTagName("script")[0]; 
