0fab0beb09ae42c3d4488a2f6eeabe1e8fb8dfc6
Pylogmon <pylogmon@outlook.com>
2023-10-19 20:52:29 +0800

    Create baidu.js

---
+++ static/js/baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?3132de7130a225150415222801ddb498";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
